bf2ba1eab62ada61e7e3f87cc24f68b93a5d1af1
nima200 <nima_ap@outlook.com>
2018-04-29 21:26:27 -0400

    version 6.0.4

--- release.sh
+++ release.sh
@@ -17,14 +17,19 @@ echo "Version: ${version}"
 # Build
 ./build.sh
 
-# Tag
+# Tag Github
 git add -A
 git commit -m "version ${version}"
 git tag -a "${version}" -m "version ${version}"
 git push origin development
 git push origin development --tags
 
+# Update the image tags
 docker tag ${GCR}/${PROJECT}/${IMAGE}:latest ${GCR}/${PROJECT}/${IMAGE}:${version}
 
+# Push to docker repo on g-cloud
 docker push ${GCR}/${PROJECT}/${IMAGE}:latest
 docker push ${GCR}/${PROJECT}/${IMAGE}:${version}
+
+# Update deployment
+kubectl set image deployment/hackboard hackboard=${GCR}/${PROJECT}/${IMAGE}:latest
