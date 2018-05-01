748faa00da3616543e509c9adbaa6a9d52107ea9
nima200 <nima_ap@outlook.com>
2018-04-30 22:53:02 -0400

    version 0.0.6

--- release.sh
+++ release.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 # Variables
 GCR=gcr.io
 PROJECT=hackboard6
 IMAGE=hackboard
+MODE=$1
 
 BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" || BRANCH="(unnamed branch)"     # detached HEAD
 BRANCH=${BRANCH##refs/heads/}
 
+if [ -z "${MODE}" ]; then
+    echo "Error: Expected a mode as first argument!"
+    echo "Options: "
+    echo "	* patch"
+    echo "	* minor"
+    echo "	* major"
+    echo "Please try again."
+    exit 1
+fi
+
+if [ ! ${MODE} = "patch" && ! ${MODE} = "minor" && ! ${MODE} = "major" ]; then
+    echo "Error: Expected one of the following modes:"
+    echo "	* patch"
+    echo "	* minor"
+    echo "	* major"
+    exit 1
+fi
+
 # Only allow release on master branch
+
 if [ ! ${BRANCH} = "master" ]; then
     echo "Current branch: ${BRANCH}"
     echo "Release operation is only available on master branch..."
@@ -41,4 +61,4 @@ docker push ${GCR}/${PROJECT}/${IMAGE}:latest
 docker push ${GCR}/${PROJECT}/${IMAGE}:${version}
 
 # Update deployment image on Kubernetes cluster
-kubectl set image deployment/hackboard hackboard=${GCR}/${PROJECT}/${IMAGE}:latest
+kubectl set image deployment/hackboard hackboard=${GCR}/${PROJECT}/${IMAGE}:${version}
