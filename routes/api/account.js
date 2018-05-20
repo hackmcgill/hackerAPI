7826b9e96730353e20c00eb50c824cb0a8ad47c3
Richard Zhang <richardzhang97@gmail.com>
2018-05-20 16:32:42 -0700

    Lint Stuff

--- routes/api/account.js
+++ routes/api/account.js
@@ -1,30 +1,36 @@
+"use strict";
 /* express is required to create a new route node */
-const express = require('express');
+const express = require("express");
+
+const Services = {
+    Logger: require("../../services/logger"),
+    Account: require("../../services/account")
+};
 
 module.exports = {
-    activate: function(apiRouter) {
-        const accountRouter = express.Router();
+    activate: function (apiRouter) {
+        const accountRouter = new express.Router();
 
         accountRouter.route("/").get(
             function (req, res) {
                 if (req.body.eventObjectResult.rows.length > 0) {
                     return res.status(200).json({
-                        message: 'found some events in this zip code',
+                        message: "found some events in this zip code",
                         data: {
                             events: req.body.eventObjectResult.rows
                         }
                     });
                 } else {
                     return res.status(200).json({
-                        message: 'found no events in this zip code',
+                        message: "found no events in this zip code",
                         data: {
                             events: []
                         }
-                    })
+                    });
                 }
             }
         );
 
         apiRouter.use("/account", accountRouter);
     }
-}
+};
