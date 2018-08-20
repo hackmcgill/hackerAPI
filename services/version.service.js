fe6ad7f7daf2ff926187fdb0130129a638e28a9d
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-08-20 15:21:07 -0400

    apidoc generation
    We have an auto-generated doc!

--- services/version.service.js
+++ services/version.service.js
@@ -1,8 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+"use strict";
+const fs = require("fs");
+const path = require("path");
 
 const get = function() {
-	const version = fs.readFileSync(path.join(__dirname, '../VERSION'), 'utf8');
+	const version = fs.readFileSync(path.join(__dirname, "../VERSION"), "utf8");
 	return version.toString();
 };
 
