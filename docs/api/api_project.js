f027a253d650db040f667e5252b8694346089ad2
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2018-11-28 22:49:39 -0500

    Update docs formatting

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "hackerAPI",  "version": "0.0.8",  "description": "Documentation for the API used for mchacks",  "defaultVersion": "0.0.8",  "title": "hackerAPI documentation",  "url": "https://mchacks.ca/api",  "sampleUrl": "https://mchacks.ca/api",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-11-29T02:39:05.040Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({
+  "name": "hackerAPI",
+  "version": "0.0.8",
+  "description": "Documentation for the API used for mchacks",
+  "defaultVersion": "0.0.8",
+  "title": "hackerAPI documentation",
+  "url": "https://mchacks.ca/api",
+  "sampleUrl": "https://mchacks.ca/api",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-11-29T02:39:05.040Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
