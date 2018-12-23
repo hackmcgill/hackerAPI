719cc5209cec420d155ccdc11a030990cc05bbca
Richard Zhang <richardzhang97@gmail.com>
2018-12-23 15:54:23 -0800

    change all name and pronoun validator to ascii validator

--- middlewares/validators/sponsor.validator.js
+++ middlewares/validators/sponsor.validator.js
@@ -9,7 +9,7 @@ module.exports = {
         // assuming that the tiers are between 0 and 5 (inclusive)
         // 5 is the custom class
         VALIDATOR.integerValidator("body", "tier", false, 0, 5),
-        VALIDATOR.nameValidator("body", "company", false),
+        VALIDATOR.asciiValidator("body", "company", false),
         VALIDATOR.urlValidator("body", "contractURL", false),
         VALIDATOR.mongoIdArrayValidator("body", "nominees", true),
     ],
