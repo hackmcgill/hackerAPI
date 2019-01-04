c9f4c1da34707b21db86bfae5c5cd0c432438a45
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-01-04 17:51:34 +0100

    Fix bug where new Team validator looked for name, not teamName

--- middlewares/validators/team.validator.js
+++ middlewares/validators/team.validator.js
@@ -4,7 +4,7 @@ const Constants = require("../../constants/general.constant");
 
 module.exports = {
     newTeamValidator: [
-        VALIDATOR.asciiValidator("body", "name", false),
+        VALIDATOR.asciiValidator("body", "teamName", false),
         // members by mongoID if the team creator is able to provide
         VALIDATOR.mongoIdArrayValidator("body", "members", true),
         VALIDATOR.regexValidator("body", "devpostURL", true, Constants.DEVPOST_REGEX),
