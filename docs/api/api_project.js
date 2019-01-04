1dc3a48f5c9e32c2fc218d78f9b05355e8f5f86f
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-01-04 17:53:15 +0100

    Update docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -9,8 +9,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-29T03:29:50.185Z",
+    "time": "2019-01-04T16:52:33.543Z",
     "url": "http://apidocjs.com",
-    "version": "0.17.7"
+    "version": "0.17.6"
   }
 });
