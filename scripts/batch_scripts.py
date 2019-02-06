95fe9686341ebcef6147662b37ef8e978277a416
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-02-06 15:20:15 -0500

    Feature/batch actions (#335)
    * Update batch scripts, clean up scripts
    
    * Add log verbosities
    
    * Add account invites
    
    * Add getHackers action, wrap script in try / catch
    
    * Add readline

--- scripts/batch_scripts.py
+++ scripts/batch_scripts.py
@@ -1,12 +1,15 @@
 #!/bin/bash/python3
 import base64
 from bson import ObjectId
+import csv
 import getpass
 import json
 import os
+import readline
 import requests
 import subprocess
 from typing import Any, Callable, List
+import sys
 
 # Constants
 VALID_STATUSES = {
@@ -21,8 +24,18 @@ BATCH_ACTIONS = {
     '1': 'updateStatus',
     '2': 'dayOf',
     '3': 'weekOf',
-    '4': 'downloadResume'
+    '4': 'downloadResume',
+    '5': 'inviteUsers',
+    '6': 'getHackers'
 }
+LOG_VERBOSITIES = {
+    '0': 'None',
+    '1': 'Error',
+    '2': 'Warning',
+    '3': 'Info'
+}
+
+CHOSEN_VERBOSITY = 3
 
 
 API_URL = 'https://api.mchacks.ca'
@@ -31,10 +44,26 @@ API_URL = 'https://api.mchacks.ca'
 s = requests.Session()
 
 
+def _print(msg, msgType=3, index=None, total=None):
+    """
+    Wrapper around print function such that we only print to the granularity that the user wants.
+    Also, allow for formatting of sequential print statements ex: (10/114)
+    """
+    global CHOSEN_VERBOSITY
+    if msgType <= CHOSEN_VERBOSITY:
+        out_file = sys.stdout if msgType > 1 else sys.stderr
+        if index is None or total is None:
+            print('{0}: {1}'.format(
+                LOG_VERBOSITIES[str(msgType)], msg), file=out_file)
+        else:
+            print('({0}/{1}) {2}: {3}'.format(index, total,
+                                              LOG_VERBOSITIES[str(msgType)], msg), file=out_file)
+
+
 def requestUntilSuccess(
     string: str,
     invalid_msg: str = 'Invalid input',
-    validInput: Callable[[Any], bool] = lambda x: True,
+    validInput: Callable[[Any], bool] = lambda x: x is not None,
     transformInput: Callable[[str], Any] = lambda x: x
 ) -> str:
     """
@@ -55,13 +84,13 @@ def login(session=requests.Session()):
     Logs in a user to the inputted session, and returns the session.
     """
     global API_URL
-    print("Enter target API")
-    user_input = input('Target API (Default {0}): '.format(API_URL))
+    user_input = requestUntilSuccess(
+        'Enter Target API (Default {0}): '.format(API_URL))
     if user_input != '':
         API_URL = user_input
     # Get credentials
-    print("Enter credentials for", API_URL)
-    username = input("Email: ")
+    username = requestUntilSuccess(
+        'Enter credentials for {0}: '.format(API_URL))
     logged_in = False
 
     while not logged_in:
@@ -77,6 +106,50 @@ def login(session=requests.Session()):
             print('Logged in as {0}'.format(username))
             logged_in = True
     return session
+
+
+def chooseLogVerbosity():
+    global CHOSEN_VERBOSITY
+    verbosity_list = ['{0}: {1}\n'.format(k, v)
+                      for k, v in LOG_VERBOSITIES.items()]
+    chosen_verbosity = requestUntilSuccess(
+        'Input log verbosity (default {0}):\n{1}'.format(
+            CHOSEN_VERBOSITY, ''.join(verbosity_list)),
+        'Invalid verbosity',
+        lambda x: x == '' or x in LOG_VERBOSITIES.keys(),
+        lambda x: CHOSEN_VERBOSITY if x == '' else int(x)
+    )
+    CHOSEN_VERBOSITY = chosen_verbosity
+
+
+def loadInvites() -> List[str]:
+    """
+    Load the list of invites provided by user inputted file path.
+    """
+    # Get information about batch actions
+    invite_file = requestUntilSuccess(
+        'Path to Invite CSV: ',
+        'Invalid file',
+        lambda x: x is not None and os.path.isfile(x) and os.access(x, os.R_OK)
+    )
+
+    invites = []
+    with open(invite_file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        index = 1
+        for row in reader:
+            index = index + 1
+            email = row['email']
+            accountType = row['accountType']
+            if email is not None and accountType is not None:
+                invites.append({
+                    'email': email,
+                    'accountType': accountType
+                })
+            else:
+                _print('{2} is not a valid invite format'.format(
+                    row), 1, index, len(reader))
+    return invites
 
 
 def loadIDs() -> List[str]:
@@ -99,8 +172,8 @@ def loadIDs() -> List[str]:
             if ObjectId.is_valid(r):
                 ids.append(r)
             else:
-                print(
-                    '({0}/{1}) Error: {2} is not a valid ObjectID'.format(index, len(rows), r))
+                _print('{2} is not a valid ObjectID'.format(
+                    r), 1, index, len(rows))
     # remove duplicates
     ids = list(set(ids))
     return ids
@@ -140,11 +213,11 @@ def batchAction() -> str:
 
 
 def getHacker(ID):
-    r1 = s.get('{0}/api/hacker/{1}'.format(API_URL, ID))
-    if r1.status_code != 200:
+    r = s.get('{0}/api/hacker/{1}'.format(API_URL, ID))
+    if r.status_code != 200:
         return None
     else:
-        hackerInfo = json.loads(r1.content)['data']
+        hackerInfo = json.loads(r.content)['data']
         return hackerInfo
 
 
@@ -153,6 +226,20 @@ def hasValidStatus(status, hackerInfo):
         return True
     else:
         return False
+
+
+def search(model: str = 'hacker', query=[], expand: bool = True):
+    q = json.dumps(query)
+    expand = 'true' if expand else 'false'
+    r = s.get(
+        '{0}/api/search?model={1}&q={2}&expand={3}'.format(API_URL, model, q, expand))
+    if r.status_code != 200:
+        _print('Could not perform search (Status code {0})'.format(
+            r.status_code), 1)
+        return []
+    else:
+        results = json.loads(r.content)['data']
+        return results
 
 
 def updateStatus():
@@ -168,17 +255,17 @@ def updateStatus():
             r = s.patch('{0}/api/hacker/status/{1}'.format(API_URL, ID),
                         {"status": NEW_STATUS})
             if r.status_code != 200:
-                print(
-                    '({0}/{1}) ERROR cannot update status for {2}'.format(index, len(HACKER_IDs), ID))
+                _print('cannot update status for {0}'.format(
+                    ID), 1, index, len(HACKER_IDs))
             else:
-                print(
-                    '({0}/{1}) {2} {3}'.format(index, len(HACKER_IDs), NEW_STATUS, ID))
+                _print('{0} {1}'.format(
+                    NEW_STATUS, ID), 3, index, len(HACKER_IDs))
         elif hacker is not None:
-            print(
-                '({0}/{1}) ERROR invalid status for {2}'.format(index, len(HACKER_IDs), ID))
+            _print('invalid status for {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
         else:
-            print(
-                '({0}/{1}) ERROR could not find {2}'.format(index, len(HACKER_IDs), ID))
+            _print('could not find {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
 
 
 def sendDayOfEmail():
@@ -193,17 +280,17 @@ def sendDayOfEmail():
             r = s.post(
                 '{0}/api/hacker/email/dayOf/{1}'.format(API_URL, ID))
             if r.status_code != 200:
-                print(
-                    '({0}/{1}) ERROR cannot send email to {2}'.format(index, len(HACKER_IDs), ID))
+                _print('cannot send email to {0}'.format(
+                    ID), 1, index, len(HACKER_IDs))
             else:
-                print(
-                    '({0}/{1}) Sent email to {2}'.format(index, len(HACKER_IDs), ID))
+                _print('Sent email to {0}'.format(
+                    ID), 3, index, len(HACKER_IDs))
         elif hacker is not None:
-            print(
-                '({0}/{1}) ERROR invalid status for {2}'.format(index, len(HACKER_IDs), ID))
+            _print('Sent invalid status for {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
         else:
-            print(
-                '({0}/{1}) ERROR could not find {2}'.format(index, len(HACKER_IDs), ID))
+            _print('Could not find {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
 
 
 def sendWeekOfEmail():
@@ -218,17 +305,17 @@ def sendWeekOfEmail():
             r = s.post(
                 '{0}/api/hacker/email/weekOf/{1}'.format(API_URL, ID))
             if r.status_code != 200:
-                print(
-                    '({0}/{1}) ERROR cannot send email to {2}'.format(index, len(HACKER_IDs), ID))
+                _print('Cannot send email to {0}'.format(
+                    ID), 1, index, len(HACKER_IDs))
             else:
-                print(
-                    '({0}/{1}) Sent email to {2}'.format(index, len(HACKER_IDs), ID))
+                _print('Sent email to {0}'.format(
+                    ID), 3, index, len(HACKER_IDs))
         elif hacker is not None:
-            print(
-                '({0}/{1}) ERROR invalid status for {2}'.format(index, len(HACKER_IDs), ID))
+            _print('Invalid status for {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
         else:
-            print(
-                '({0}/{1}) ERROR could not find {2}'.format(index, len(HACKER_IDs), ID))
+            _print('Could not find {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
 
 
 def downloadResume():
@@ -245,35 +332,95 @@ def downloadResume():
             r = s.get(
                 '{0}/api/hacker/resume/{1}'.format(API_URL, ID))
             if r.status_code != 200:
-                print(
-                    '({0}/{1}) ERROR {2} does not have a resume (Status code {3})'.format(index, len(HACKER_IDs), ID, r.status_code))
+                _print('Could not find resume for {0}'.format(
+                    ID), 1, index, len(HACKER_IDs))
             else:
                 resume = json.loads(r.content)['data']['resume'][0]['data']
                 download_path = "{0}/{1}.pdf".format(DOWNLOAD_DIR, ID)
                 with open(download_path, "wb") as fh:
                     byte_data = bytearray(resume)
                     fh.write(byte_data)
-                print(
-                    '({0}/{1}) Downloaded resume from {2} to {3}'.format(index, len(HACKER_IDs), ID, download_path))
+                _print('Downloaded resume for {0} to {1}'.format(
+                    ID, download_path), 3, index, len(HACKER_IDs))
         elif hacker is not None:
-            print(
-                '({0}/{1}) ERROR invalid status for {2}'.format(index, len(HACKER_IDs), ID))
+            _print('invalid status for {0}'.format(
+                ID), 3, index, len(HACKER_IDs))
         else:
-            print(
-                '({0}/{1}) ERROR could not find {2}'.format(index, len(HACKER_IDs), ID))
+            _print('Could not find hacker {0}'.format(
+                ID), 1, index, len(HACKER_IDs))
+
+
+def inviteUsers():
+    INVITES = loadInvites()
+    for index, invite in enumerate(INVITES):
+        index = index + 1
+        r = s.post(
+            '{0}/api/account/invite/'.format(API_URL), invite)
+        if r.status_code != 200:
+            _print('Could not invite {0}, {1}'.format(
+                invite['email'], invite['accountType']), 1, index, len(INVITES))
+        else:
+            _print('Invited {0}, {1}'.format(
+                invite['email'], invite['accountType']), 3, index, len(INVITES))
+
+
+def getHackers():
+    CUR_STATUS = status('current')
+    DOWNLOAD_DIR = getDownloadDirectory()
+    results = search(
+        'hacker',
+        [{'param': 'status', 'operation': 'equals', 'value': CUR_STATUS}]
+    )
+    with open('{0}/hackerIDs_{1}.csv'.format(DOWNLOAD_DIR, CUR_STATUS), 'w') as out_file:
+        fieldnames = [
+            'id',
+            'First Name',
+            'Last Name',
+            'Email',
+            'School',
+            'Degree',
+            'Graduation Year',
+            'Job Interest',
+            'Github',
+            'LinkedIn'
+        ]
+        csv_writer = csv.DictWriter(out_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        for result in results:
+            csv_writer.writerow({
+                'id': result['id'],
+                'First Name': result['accountId']['firstName'],
+                'Last Name': result['accountId']['lastName'],
+                'Email': result['accountId']['email'],
+                'School': result['school'],
+                'Degree': result['degree'],
+                'Graduation Year': result['graduationYear'],
+                'Job Interest': result['application']['jobInterest'],
+                'Github': result['application']['portfolioURL']['github'],
+                'LinkedIn': result['application']['portfolioURL']['linkedIn'],
+            })
 
 
 if __name__ == "__main__":
     # execute only if run as a script
+    chooseLogVerbosity()
     login(s)
     while True:
         BATCH_ACTION = batchAction()
-        if BATCH_ACTION == 'weekOf':
-            sendWeekOfEmail()
-        elif BATCH_ACTION == 'dayOf':
-            sendDayOfEmail()
-        elif BATCH_ACTION == 'updateStatus':
-            updateStatus()
-        elif BATCH_ACTION == 'downloadResume':
-            downloadResume()
-        print('Finished {0}'.format(BATCH_ACTION))
+        try:
+            if BATCH_ACTION == 'weekOf':
+                sendWeekOfEmail()
+            elif BATCH_ACTION == 'dayOf':
+                sendDayOfEmail()
+            elif BATCH_ACTION == 'updateStatus':
+                updateStatus()
+            elif BATCH_ACTION == 'downloadResume':
+                downloadResume()
+            elif BATCH_ACTION == 'inviteUsers':
+                inviteUsers()
+            elif BATCH_ACTION == 'getHackers':
+                getHackers()
+            print('Finished {0}'.format(BATCH_ACTION))
+        except Exception as e:
+            _print('Failed to perform action {0}: {1}'.format(
+                BATCH_ACTION, e), 1)
