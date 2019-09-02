b7a6e3473ef9cc672953c1b0489c22637b91aa4a
Tyrone Wong <tyronewong96@gmail.com>
2019-09-01 23:28:19 -0400

    Added closures to the parsePatch service

--- services/parsePatch.service.js
+++ services/parsePatch.service.js
@@ -1,18 +1,21 @@
 "use strict";
 const mongoose = require("mongoose");
 
-function parsePatch(req, res, next, model) {
-  let modelDetails = {};
-  for (const val in req.body) {
-    if (model.schema.paths.hasOwnProperty(val)) {
-      modelDetails[val] = req.body[val];
-      delete req.body[val];
-    }
-  }
-  req.body.modelDetails = modelDetails;
-  return next();
-}
-
 module.exports = {
-  parsePatch: parsePatch
+    parsePatch: function(req, model, done) {
+        setMatchingAttributes(model).then(() => {
+            done();
+        });
+    }
 };
+
+async function setMatchingAttributes(model) {
+    let modelDetails = {};
+    for (const val in req.body) {
+        if (model.schema.paths.hasOwnProperty(val)) {
+            modelDetails[val] = req.body[val];
+            delete req.body[val];
+        }
+    }
+    req.body.modelDetails = modelDetails;
+}
