d7730b42c8feadf3843e5dbe2d43863e986beb9f
Tyrone Wong <tyronewong96@gmail.com>
2019-09-05 23:42:21 -0400

    fixed the closure format

--- services/parsePatch.service.js
+++ services/parsePatch.service.js
@@ -2,20 +2,17 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-    parsePatch: function(req, model, done) {
-        setMatchingAttributes(model).then(() => {
-            done();
-        });
+    parsePatch: function(model) {
+        return function(req, res, next) {
+            let modelDetails = {};
+            for (const val in req.body) {
+                if (model.schema.paths.hasOwnProperty(val)) {
+                    modelDetails[val] = req.body[val];
+                    delete req.body[val];
+                }
+            }
+            req.body.modelDetails = modelDetails;
+            return next();
+        };
     }
 };
-
-async function setMatchingAttributes(model) {
-    let modelDetails = {};
-    for (const val in req.body) {
-        if (model.schema.paths.hasOwnProperty(val)) {
-            modelDetails[val] = req.body[val];
-            delete req.body[val];
-        }
-    }
-    req.body.modelDetails = modelDetails;
-}
