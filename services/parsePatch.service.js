ae3eb35c044951c9a2d5150ffab0d4cb529f8e30
Tyrone Wong <tyronewong96@gmail.com>
2019-10-15 00:06:27 -0400

    associated the model name to be the attribute in req.body in parsePatch

--- services/parsePatch.service.js
+++ services/parsePatch.service.js
@@ -1,17 +1,24 @@
 "use strict";
-const mongoose = require("mongoose");
 
 module.exports = {
-    parsePatch: function(model) {
-        return function(req, res, next) {
-            let modelDetails = {};
+    /**
+     * Takes individual attributes within req.body and groups them into a single object
+     * that houses all the attributes given by the model. Once done, it will create
+     * a new attribute req.body.{modelName} and set it as the single object
+     * @param {{*}} model A model object to contains all the attribute to match in req.body
+     * @param {string} modelName The property name to store the new object of collected properties
+     * @param {(err?)=>void} callback
+     */
+    parsePatch: function (model, modelName) {
+        return function (req, res, next) {
+            let modelAttributes = {};
             for (const val in req.body) {
                 if (model.schema.paths.hasOwnProperty(val)) {
-                    modelDetails[val] = req.body[val];
+                    modelAttributes[val] = req.body[val];
                     delete req.body[val];
                 }
             }
-            req.body.modelDetails = modelDetails;
+            req.body[modelName] = modelAttributes;
             return next();
         };
     }
