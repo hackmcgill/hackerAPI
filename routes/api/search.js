32d7ecab0dec71ebb092fc56ede98b489220e620
Harsh Patel <me@mrharshpatel.com>
2019-11-03 14:57:05 -0500

    Changed comments

--- routes/api/search.js
+++ routes/api/search.js
@@ -80,8 +80,8 @@ module.exports = {
          * @apiParam (query) {number} page the page number that you would like
          * @apiParam (query) {number} limit the maximum number of results that you would like returned
          * @apiParam (query) {any} sort_by any parameter you want to sort the results by
+         * @apiParam (query) {String} json object string containing the update fields for the defined model
          * @apiParam (query) {boolean} expand whether you want to expand sub documents within the results
-         * @apiParam (query) {String} update new status or type of email
          * 
          * @apiSuccess {String} message Success message
          * @apiSuccess {Object} data Results
@@ -116,6 +116,38 @@ module.exports = {
             Controllers.Search.searchResults
         );
 
+        /**
+         * @api {get} /search/sendEmails execute an action on a specific query for any defined model
+         * @apiName search
+         * @apiGroup Search
+         * @apiVersion 0.0.8
+         *
+         * @apiParam (query) {String} model the model to be searched
+         * @apiParam (query) {Array} q the query to be executed. For more information on how to format this, please see https://docs.mchacks.ca/architecture/
+         * @apiParam (query) {String} model the model to be searched
+         * @apiParam (query) {String} sort either "asc" or "desc"
+         * @apiParam (query) {number} page the page number that you would like
+         * @apiParam (query) {number} limit the maximum number of results that you would like returned
+         * @apiParam (query) {any} sort_by any parameter you want to sort the results by
+         * @apiParam (query) {String} |
+         * @apiParam (query) {boolean} expand whether you want to expand sub documents within the results
+         * 
+         * @apiSuccess {String} message Success message
+         * @apiSuccess {Object} data Results
+         * @apiSuccessExample {object} Success-Response:
+         *      {
+                    "message": "Hacker update emails sent.",
+                    "data": {}
+                }
+         *
+         * @apiSuccess {String} message Error message
+         * @apiSuccess {Object} data Specific Error message
+         * @apiSuccessExample {object} Success-Response:
+         *      {
+                    "message": "Error while generating email",
+                    "data": "Email service failed."
+                }
+         */
         searchRouter.route("/sendEmails").get(
             Middleware.Auth.ensureAuthenticated(),
             Middleware.Auth.ensureAuthorized(),
