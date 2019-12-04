61120d5005240499eb57763b1b5da55e2148a911
logan-r <logan.ralston@mail.mcgill.ca>
2019-12-03 21:19:50 -0500

    Fix bug with wrong validator for being used for gender

--- middlewares/validators/account.validator.js
+++ middlewares/validators/account.validator.js
@@ -9,7 +9,7 @@ module.exports = {
         VALIDATOR.stringValidator("body", "pronoun", false),
         VALIDATOR.regexValidator("body", "email", false, Constants.EMAIL_REGEX),
         VALIDATOR.alphaArrayValidator("body", "dietaryRestrictions", false),
-        VALIDATOR.enumValidator("body", "gender", false),
+        VALIDATOR.stringValidator("body", "gender", false),
         VALIDATOR.passwordValidator("body", "password", false),
         VALIDATOR.jwtValidator("header", "token", process.env.JWT_CONFIRM_ACC_SECRET, true),
         VALIDATOR.dateValidator("body", "birthDate", false),
@@ -21,7 +21,7 @@ module.exports = {
         VALIDATOR.stringValidator("body", "pronoun", true),
         VALIDATOR.regexValidator("body", "email", true, Constants.EMAIL_REGEX),
         VALIDATOR.alphaArrayValidator("body", "dietaryRestrictions", true),
-        VALIDATOR.enumValidator("body", "gender", true),
+        VALIDATOR.stringValidator("body", "gender", true),
         VALIDATOR.dateValidator("body", "birthDate", true),
         VALIDATOR.phoneNumberValidator("body", "phoneNumber", true)
     ],
