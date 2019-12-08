7728ab01ab89de23b32cb0693dad7c0f6492f553
Loreina Chew <loreina.chew@gmail.com>
2019-12-08 13:30:49 -0500

    Revert "revert formatting changes to docs folder"
    This reverts commit f4954b6a86cd4e03acfd96b05a36a0c36e868ad4.

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,16 +1,16 @@
 define({
-  "name": "hackerAPI",
-  "version": "0.0.8",
-  "description": "Documentation for the API used for mchacks",
-  "defaultVersion": "0.0.8",
-  "title": "hackerAPI documentation",
-  "url": "https://api.mchacks.ca/api",
-  "sampleUrl": "https://api.mchacks.ca/api",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-05-06T17:36:31.903Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.7"
-  }
+    name: "hackerAPI",
+    version: "0.0.8",
+    description: "Documentation for the API used for mchacks",
+    defaultVersion: "0.0.8",
+    title: "hackerAPI documentation",
+    url: "https://api.mchacks.ca/api",
+    sampleUrl: "https://api.mchacks.ca/api",
+    apidoc: "0.3.0",
+    generator: {
+        name: "apidoc",
+        time: "2019-05-06T17:36:31.903Z",
+        url: "http://apidocjs.com",
+        version: "0.17.7"
+    }
 });
