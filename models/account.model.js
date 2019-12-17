fd5b855f19c66fda8959fe87b994e90c5b6c7cd4
Harsh Patel <me@mrharshpatel.com>
2019-12-16 20:49:50 -0500

    Update account.model.js

--- models/account.model.js
+++ models/account.model.js
@@ -2,7 +2,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Constants = require("../constants/general.constant");
-const Error = require("../constants/error.constant")
 
 //describes the data type
 const AccountSchema = new mongoose.Schema({
