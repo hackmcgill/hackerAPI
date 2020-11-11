0e093c991a16320cbf3991b5d07a9f30e93f9df4
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2020-11-10 19:24:17 -0500

    feat: add settings to seed script

---
+++ seed/settings.seed.js
@@ -0,0 +1,22 @@
+"use strict";
+const Settings = require("../models/settings.model");
+
+/**
+ * Drops all elements in Role
+ */
+function drop() {
+    return Settings.deleteMany({});
+}
+
+/**
+ * Stores all of the roles in the db
+ * @param {Settings} setting the setting that we want to seed
+ */
+function store(setting) {
+    return Settings.collection.insertOne(setting);
+}
+
+module.exports = {
+    store: store,
+    drop: drop
+};
