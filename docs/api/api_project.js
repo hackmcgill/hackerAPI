7a89036c7047e117d572911b1cc42a23aa6df5af
meldunn <46253456+meldunn@users.noreply.github.com>
2021-06-14 23:39:07 -0400

    automatically generate postman collections
    uses apidoc-postman to generate api collection from api docs
    
    Co-Authored-By: Kyle Rubenok <2529120+krubenok@users.noreply.github.com>

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,1 +1,16 @@
-define({  "name": "hackerAPI",  "version": "0.0.8",  "description": "Documentation for the API used for mchacks",  "defaultVersion": "0.0.8",  "title": "hackerAPI documentation",  "url": "https://api.mchacks.ca/api",  "sampleUrl": "https://api.mchacks.ca/api",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2020-11-01T22:01:33.009Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "hackerAPI",
+  "version": "0.0.8",
+  "description": "Documentation for the API used for mchacks",
+  "defaultVersion": "0.0.8",
+  "title": "hackerAPI documentation",
+  "url": "https://api.mchacks.ca/api",
+  "sampleUrl": "https://api.mchacks.ca/api",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-06-15T03:37:22.945Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
