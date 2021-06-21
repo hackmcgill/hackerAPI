0df91eb7473448f0f9bc28489a83e1a52600de82
James Xu <36768789+jamesxu123@users.noreply.github.com>
2021-06-20 20:24:11 -0400

    Update account.test.js

--- tests/account.test.js
+++ tests/account.test.js
@@ -1,6 +1,6 @@
 "use strict";
 const chai = require("chai");
-chai.Assertion.includeStack = true
+chai.config.includeStack = true
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const server = require("../app");
