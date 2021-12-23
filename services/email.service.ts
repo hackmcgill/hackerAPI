f2efdaf8b289911db0fdc0dc773fe281ce041e21
Anmol Brar <brarsanmol@protonmail.com>
2021-12-22 23:31:57 -0500

    refactor: change e-mail service to use nodemailer.
    - BENEFITS:
      - prevent's vendor lock-in as before we were using sendgrid's proprietary library.
        - this will help prevent incidents such as what occurred on december 1st - 7th.
          - in essence allow's us to use any smtp mailing service as long as we swap the information in the .env file.
      - allow's developers to catch mail using mailcatcher, mailtrap, etc...
        - (no longer having to distribute sendgrid key to devs)
        - no longer have to go to the database to get account confirmation tokens, or password reset tokens.
    
    - TODO:
      - implement a *.mailer.ts pattern.
        - authentication.mailer.ts
          - send forgot password, account confirmation, etc...
        - BENEFITS:
          - will clean up the code in the controller layer.
          - will establish clean boundaries between the mailing layer and controller layer, improving the codebase overall.

--- services/email.service.ts
+++ services/email.service.ts
@@ -1,162 +1,55 @@
-import client from "@sendgrid/mail";
 import { readFileSync } from "fs";
-import { join } from "path";
-const Constants = require("../constants/general.constant");
-import * as Handlebars from "handlebars";
 import { autoInjectable } from "tsyringe";
 import { EnvService } from "./env.service";
+import { createTransport } from "nodemailer";
+import { compile } from "handlebars";
+import mjml2html from "mjml";
+import { LoggerService } from "./logger.service";
 
 @autoInjectable()
 export class EmailService {
-    constructor(private readonly envService: EnvService) {
-        client.setApiKey(this.envService.get("SENDGRID_API_KEY") ?? "");
+    protected readonly mailer;
+
+    constructor(
+        private readonly envService: EnvService,
+        private readonly loggerService: LoggerService
+    ) {
+        this.mailer = createTransport({
+            host: this.getEmailAttribute("HOST"),
+            port: parseInt(this.getEmailAttribute("PORT")!),
+            auth: {
+                user: this.getEmailAttribute("USERNAME"),
+                pass: this.getEmailAttribute("PASSWORD")
+            }
+        });
+        this.mailer
+            .verify()
+            .catch((error) =>
+                this.loggerService
+                    .getLogger()
+                    .error(`Failed to connect to SMTP server. Error: ${error}`)
+            );
     }
 
-    /**
-     * Send one email
-     * @param {*} mailData
-     * @param {(err?)=>void} callback
-     */
-    send(mailData: any, callback = (error?: Object) => {}) {
-        if (this.envService.isTest()) {
-            //Silence all actual emails if we're testing
-            mailData.mailSettings = {
-                sandboxMode: {
-                    enable: true
-                }
-            };
-        }
-        return client
-            .send(mailData, false)
-            .then((response) => {
-                callback();
-                return response;
+    public async send({ ...args }: any, { ...context }: any, callback?: any) {
+        args.from = this.envService.get("NO_REPLY_EMAIL");
+        if (args.html)
+            args.html = compile(
+                mjml2html(readFileSync(args.html, "utf-8")).html
+            )(context);
+        await this.mailer
+            .sendMail({
+                ...args,
+                from: this.envService.get("NO_REPLY_EMAIL")
             })
-            .catch((error) => {
-                callback(error);
-            });
-    }
-    /**
-     * Send separate emails to the list of users in mailData
-     * @param {*} mailData
-     * @param {(err?)=>void} callback
-     */
-    sendMultiple(mailData: any, callback = (error?: Object) => {}) {
-        return client
-            .sendMultiple(mailData)
-            .then((response) => {
-                callback();
-                return response;
-            })
-            .catch((error) => {
-                callback(error);
-            });
-    }
-    /**
-     * Send email with ticket.
-     * @param {string} firstName the recipient's first name
-     * @param {string} recipient the recipient's email address
-     * @param {string} ticket the ticket image (must be base-64 string)
-     * @param {(err?)=>void} callback
-     */
-    sendWeekOfEmail(
-        firstName: string,
-        recipient: string,
-        ticket: string,
-        callback = (error?: Object) => {}
-    ) {
-        const handlebarsPath = join(__dirname, `../assets/email/Ticket.hbs`);
-        const html = this.renderEmail(handlebarsPath, {
-            firstName: firstName,
-            ticket: ticket
-        });
-        const mailData = {
-            to: recipient,
-            from: this.envService.get("NO_REPLY_EMAIL"),
-            subject: Constants.EMAIL_SUBJECTS[Constants.WEEK_OF],
-            html: html
-        };
-        this.send(mailData).then((response) => {
-            if (
-                !response ||
-                (response[0].statusCode >= 200 && response[0].statusCode < 300)
-            ) {
-                callback();
-            } else {
-                callback(response[0]);
-            }
-        }, callback);
-    }
-    /**
-     * Send email with ticket.
-     * @param {string} firstName the recipient's first name
-     * @param {string} recipient the recipient's email address
-     * @param {(err?)=>void} callback
-     */
-    sendDayOfEmail(
-        firstName: string,
-        recipient: string,
-        callback = (error?: Object) => {}
-    ) {
-        const handlebarsPath = join(__dirname, `../assets/email/Welcome.hbs`);
-        const html = this.renderEmail(handlebarsPath, {
-            firstName: firstName
-        });
-        const mailData = {
-            to: recipient,
-            from: this.envService.get("NO_REPLY_EMAIL"),
-            subject: Constants.EMAIL_SUBJECTS[Constants.WEEK_OF],
-            html: html
-        };
-        this.send(mailData).then((response) => {
-            if (
-                !response ||
-                (response[0].statusCode >= 200 && response[0].statusCode < 300)
-            ) {
-                callback();
-            } else {
-                callback(response[0]);
-            }
-        }, callback);
+            .catch((error) => callback(error));
     }
 
-    sendStatusUpdate(
-        firstName: string,
-        recipient: string,
-        status: string,
-        callback = (error?: Object) => {}
-    ) {
-        const handlebarsPath = join(
-            __dirname,
-            `../assets/email/statusEmail/${status}.hbs`
-        );
-        const mailData = {
-            to: recipient,
-            from: this.envService.get("NO_REPLY_EMAIL"),
-            subject: Constants.EMAIL_SUBJECTS[status],
-            html: this.renderEmail(handlebarsPath, {
-                firstName: firstName
-            })
-        };
-        this.send(mailData).then((response) => {
-            if (
-                !response ||
-                (response[0].statusCode >= 200 && response[0].statusCode < 300)
-            ) {
-                callback();
-            } else {
-                callback(response[0]);
-            }
-        }, callback);
-    }
-    /**
-     * Generates the HTML from the handlebars template file found at the given path.
-     * @param {string} path the absolute path to the handlebars template file
-     * @param {*} context any variables that need to be replaced in the template file
-     */
-    renderEmail(path: string, context: Object) {
-        const templateStr = readFileSync(path).toString();
-        const template = Handlebars.compile(templateStr);
-        return template(context);
+    private getEmailAttribute(name: string) {
+        return this.envService.isDevelopment()
+            ? this.envService.get(`EMAIL_${name}_DEV`)
+            : this.envService.isProduction()
+            ? this.envService.get(`EMAIL_${name}_DEPLOY`)
+            : this.envService.get(`EMAIL_${name}_TEST`);
     }
 }
