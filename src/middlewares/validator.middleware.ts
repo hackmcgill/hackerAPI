7b74fe07f23ff680f6b45d62f73e33c243edfd63
Anmol Brar <brarsanmol@protonmail.com>
2022-02-03 05:19:06 -0500

    feat: validation middleware for models
    - leverage class-validator to validate against entities.
    - retrieve errors and flatmap them to allow for a clean output back
      to the dashboard.

---
+++ src/middlewares/validator.middleware.ts
@@ -0,0 +1,35 @@
+import { Middleware } from "@decorators/express";
+import { plainToClass } from "class-transformer";
+import { validateOrReject } from "class-validator";
+import { Request, Response, NextFunction } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+import * as ErrorConstants from "@constants/error.constant";
+
+export function Validator(model: any): any {
+    class ValidatorMiddleware implements Middleware {
+        async use(
+            request: Request<
+                ParamsDictionary,
+                any,
+                any,
+                ParsedQs,
+                Record<string, any>
+            >,
+            response: Response<any, Record<string, any>>,
+            next: NextFunction
+        ): Promise<void> {
+            await validateOrReject(plainToClass(model, request.body))
+                .then(() => next())
+                .catch((errors) => {
+                    response.status(422).send({
+                        message: ErrorConstants.VALIDATION_422_MESSAGE,
+                        errors: errors.flatMap((error: any) =>
+                            Object.values(error["constraints"])
+                        )
+                    });
+                });
+        }
+    }
+    return new ValidatorMiddleware();
+}
